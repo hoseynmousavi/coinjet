03dfaf6891cfd786153a71ecb3a3412acb0def53
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-09-27 23:24:22 +0330

    initial

--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -3,28 +3,28 @@ function rootRouter(app)
     app.route("*")
         .get((req, res) =>
         {
-            console.log("get body", req?.body)
-            console.log("get query", req?.query)
+            console.log("get body: " + req.originalUrl, req?.body)
+            console.log("get query: " + req.originalUrl, req?.query)
             res.send({message: "OK"})
         })
         .post((req, res) =>
         {
-            console.log("post", req?.body)
+            console.log("post: " + req.originalUrl, req?.body)
             res.send({message: "OK"})
         })
         .put((req, res) =>
         {
-            console.log("put", req?.body)
+            console.log("put: " + req.originalUrl, req?.body)
             res.send({message: "OK"})
         })
         .patch((req, res) =>
         {
-            console.log("patch", req?.body)
+            console.log("patch: " + req.originalUrl, req?.body)
             res.send({message: "OK"})
         })
         .delete((req, res) =>
         {
-            console.log("delete", req?.body)
+            console.log("delete: " + req.originalUrl, req?.body)
             res.send({message: "OK"})
         })
 }
