7b8be3d56c432a02e242939820baa1d425aa518c
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-09-29 17:01:21 +0330

    fx

--- src/controllers/tradeController.js
+++ src/controllers/tradeController.js
@@ -5,36 +5,70 @@ import e from "express"
 
 function getUser(req, res)
 {
+    const sendBody = {...req.body}
+    delete sendBody.api
+    delete sendBody.method
+    const body = Object.keys(sendBody).length ? JSON.stringify(sendBody) : ""
     const base = "https://api.kucoin.com"
-    const api = "/api/v1/sub/user"
+    const api = req.body.api
+    const method = req.body.method.toUpperCase()
 
     const timeStamp = Math.floor(new Date().getTime())
-    console.log(timeStamp)
     const passPhrase = crypto.createHmac("sha256", data.kuCoinSecret).update(data.kuCoinPassphrase).digest("base64")
-    const sign = crypto.createHmac("sha256", data.kuCoinSecret).update(timeStamp + "GET" + api + "").digest("base64")
+    const sign = crypto.createHmac("sha256", data.kuCoinSecret).update(timeStamp + method + api + body).digest("base64")
 
-    axios.get(
-        base + api,
-        {
-            headers: {
-                "KC-API-KEY": data.kuCoinKey,
-                "KC-API-SIGN": sign,
-                "KC-API-TIMESTAMP": timeStamp,
-                "KC-API-PASSPHRASE": passPhrase,
-                "KC-API-KEY-VERSION": data.kuCoinApiVersion,
+    if (method === "GET")
+    {
+        axios.get(
+            base + api,
+            {
+                params: sendBody,
+                headers: {
+                    "KC-API-KEY": data.kuCoinKey,
+                    "KC-API-SIGN": sign,
+                    "KC-API-TIMESTAMP": timeStamp,
+                    "KC-API-PASSPHRASE": passPhrase,
+                    "KC-API-KEY-VERSION": data.kuCoinApiVersion,
+                },
             },
-        },
-    )
-        .then(respond =>
-        {
-            console.log("respond", respond)
-            res.status(200).send({message: "yes"})
-        })
-        .catch(err =>
-        {
-            console.log(err.response.data)
-            res.status(500).send({message: "nope"})
-        })
+        )
+            .then(respond =>
+            {
+                console.log("respond", respond)
+                res.status(200).send({message: "yes"})
+            })
+            .catch(err =>
+            {
+                console.log(err.response.data)
+                res.status(500).send({message: "nope"})
+            })
+    }
+    else
+    {
+        axios.post(
+            base + api,
+            sendBody,
+            {
+                headers: {
+                    "KC-API-KEY": data.kuCoinKey,
+                    "KC-API-SIGN": sign,
+                    "KC-API-TIMESTAMP": timeStamp,
+                    "KC-API-PASSPHRASE": passPhrase,
+                    "KC-API-KEY-VERSION": data.kuCoinApiVersion,
+                },
+            },
+        )
+            .then(respond =>
+            {
+                console.log("respond", respond)
+                res.status(200).send({message: "yes"})
+            })
+            .catch(err =>
+            {
+                console.log(err.response.data)
+                res.status(500).send({message: "nope"})
+            })
+    }
 }
 
 const tradeController = {
