297763a0eb36b0a44315d2c3bc095287bf028632
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-09-29 23:48:23 +0330

    fx

--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -3,41 +3,65 @@ import data from "../data"
 
 function rootRouter(app)
 {
-    app.route("*")
+    app.route("/")
         .post((req, res) =>
         {
             console.log("post: ", req.body)
 
-            const {message} = req.body || {}
-            const {message_id, from, chat, text} = message || {}
-            if (message_id && from && chat && text)
+            const {message, channel_post} = req.body || {}
+            if (message)
             {
-
-                const textBack = text === "/start" ?
-                    `به‌به! ببین کی اینجاس! ${from.first_name} عزیزم!`
-                    :
-                    text.includes("خوب") ?
-                        "بله، خوبم!"
+                const {message_id, from, chat, text} = message
+                if (message_id && from && chat && text)
+                {
+                    const textBack = text === "/start" ?
+                        `به‌به! ببین کی اینجاس! ${from.first_name} عزیزم!`
                         :
-                        text.includes("بای") || text.includes("خداحافظ") || text.includes("یاعلی") ?
-                            `${text}!`
+                        text.includes("خوب") ?
+                            "بله، خوبم!"
                             :
-                            text.includes("سلام") || text.includes("hi") || text.includes("hello") ?
-                                "سلام جیگر"
+                            text.includes("بای") || text.includes("خداحافظ") || text.includes("یاعلی") ?
+                                `${text}!`
                                 :
-                                `کاملاً متوجه‌ام ${from.first_name} عزیز`
+                                text.includes("سلام") || text.includes("hi") || text.includes("hello") ?
+                                    "سلام جیگر"
+                                    :
+                                    `کاملاً متوجه‌ام ${from.first_name} عزیز`
 
-                axios.post(
-                    `https://api.telegram.org/bot${data.token}/sendMessage`,
+                    axios.post(
+                        `https://api.telegram.org/bot${data.token}/sendMessage`,
+                        {
+                            chat_id: chat.id,
+                            text: textBack,
+                            reply_to_message_id: message_id,
+                            allow_sending_without_reply: true,
+                        },
+                    )
+                        .then(() => console.log("sent"))
+                        .catch(() => console.error("not sent"))
+                }
+            }
+            else if (channel_post)
+            {
+                const {message_id, author_signature, chat, text} = channel_post
+                if (message_id && author_signature && chat && text)
+                {
+                    const textBack = text.includes("سیگنال") && `گفتی سیگنال ${author_signature}؟ ایول`
+                    if (textBack)
                     {
-                        chat_id: chat.id,
-                        text: textBack,
-                        reply_to_message_id: message_id,
-                        allow_sending_without_reply: true,
-                    },
-                )
-                    .then(() => console.log("sent"))
-                    .catch(() => console.error("not sent"))
+                        axios.post(
+                            `https://api.telegram.org/bot${data.token}/sendMessage`,
+                            {
+                                chat_id: chat.id,
+                                text: textBack,
+                                reply_to_message_id: message_id,
+                                allow_sending_without_reply: true,
+                            },
+                        )
+                            .then(() => console.log("sent"))
+                            .catch(() => console.error("not sent"))
+                    }
+                }
             }
 
             res.send({message: "OK"})
