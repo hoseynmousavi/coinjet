ce39c217f43ddca550bbc0385cbe5651c2ca8431
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-11-24 19:42:25 +0330

    yes

--- src/request/urlMaker.js
+++ src/request/urlMaker.js
@@ -2,7 +2,7 @@ import data from "../data"
 
 function urlMaker({isKuCoin, isTelegram, url, param})
 {
-    return (isKuCoin ? data.kuCoinBase : isTelegram ? data.telegramApi + data.telegramToken : "") + url + "/" + param
+    return (isKuCoin ? data.kuCoinBase : isTelegram ? data.telegramApi + data.telegramToken : "") + url + (param ? "/" + param : "")
 }
 
 export default urlMaker
