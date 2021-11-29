893074a340fa7802bcc6f9798e1313f4e98ce91f
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-11-29 09:20:20 +0330

    test

--- src/controllers/telegramController.js
+++ src/controllers/telegramController.js
@@ -1,46 +1,12 @@
-import axios from "axios"
-import data from "../data"
-import request from "../request/request"
-import telegramConstant from "../constants/telegramConstant"
-import exchangeController from "./exchangeController"
+import startChat from "../helpers/telegram/startChat"
 
 function handlePvChat(message)
 {
     const {message_id, from, chat, text} = message
     if (message_id && from && chat && text)
     {
-        if (text === "/start")
-        {
-            exchangeController.getExchangesHelper().then((exchanges, err) =>
-            {
-                if (err) console.log("have err:", err)
-                else
-                {
-                    request.post({
-                        isTelegram: true, url: telegramConstant.sendMessage,
-                        data: {
-                            chat_id: chat.id,
-                            text: telegramConstant.welcomeMsg,
-                        },
-                    })
-                        .then(res => console.log({res: res?.data}))
-                        .catch(err => console.log({err: err?.response?.data}))
-
-                    request.post({
-                        isTelegram: true, url: telegramConstant.sendMessage,
-                        data: {
-                            chat_id: chat.id,
-                            text: telegramConstant.chooseExchanges,
-                            reply_markup: {keyboard: [exchanges.map(item => ({text: item.name}))], one_time_keyboard: true, resize_keyboard: true},
-                            // reply_to_message_id: message_id,
-                            // allow_sending_without_reply: true,
-                        },
-                    })
-                        .then(res => console.log({res: res?.data}))
-                        .catch(err => console.log({err: err?.response?.data}))
-                }
-            })
-        }
+        console.log(message)
+        if (text === "/start") startChat({message_id, from, chat, text})
         else
         {
             console.log("got shit")
---
+++ src/helpers/telegram/startChat.js
@@ -0,0 +1,38 @@
+import exchangeController from "../../controllers/exchangeController"
+import request from "../../request/request"
+import telegramConstant from "../../constants/telegramConstant"
+
+function startChat({message_id, from, chat, text})
+{
+    exchangeController.getExchangesHelper().then((exchanges, err) =>
+    {
+        if (err) console.log("have err:", err)
+        else
+        {
+            request.post({
+                isTelegram: true, url: telegramConstant.sendMessage,
+                data: {
+                    chat_id: chat.id,
+                    text: telegramConstant.welcomeMsg,
+                },
+            })
+                .then(res => console.log({res: res?.data}))
+                .catch(err => console.log({err: err?.response?.data}))
+
+            request.post({
+                isTelegram: true, url: telegramConstant.sendMessage,
+                data: {
+                    chat_id: chat.id,
+                    text: telegramConstant.chooseExchanges,
+                    reply_markup: {keyboard: [exchanges.map(item => ({text: item.name}))], one_time_keyboard: true, resize_keyboard: true},
+                    // reply_to_message_id: message_id,
+                    // allow_sending_without_reply: true,
+                },
+            })
+                .then(res => console.log({res: res?.data}))
+                .catch(err => console.log({err: err?.response?.data}))
+        }
+    })
+}
+
+export default startChat
