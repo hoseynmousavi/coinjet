ab2f42c40292aaded496f1ea34b1e197ef6eb228
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-12-08 11:46:40 +0330

    test

--- src/constants/routeConstant.js
+++ src/constants/routeConstant.js
@@ -2,7 +2,7 @@ const routeConstant = {
     telegram: "/",
     exchange: "/exchange",
     user: "/user",
-    kucoinMiddleWare: "kucoin-middleware"
+    kucoinMiddleWare: "/kucoin-middleware"
 }
 
 export default routeConstant
