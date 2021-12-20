7812f76ee372b1696a2728fad445af30a6151ca9
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-12-20 17:45:45 +0330

    fx

--- src/request/request.js
+++ src/request/request.js
@@ -26,6 +26,7 @@ function get({kuCoinUserExchange, nobitexUserExchange, isTelegram, url, param = 
 
 function post({kuCoinUserExchange, nobitexUserExchange, isTelegram, url, param = "", data})
 {
+    if (!!kuCoinUserExchange) console.log(urlMaker({isKuCoin: !!kuCoinUserExchange, isTelegram, url, param}))
     return axios.post(
         urlMaker({isKuCoin: !!kuCoinUserExchange, isTelegram, url, param}),
         data,
