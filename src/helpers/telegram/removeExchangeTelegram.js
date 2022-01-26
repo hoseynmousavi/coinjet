356cdf2c1be8efec3ce29a4005cd908e31c446de
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-01-26 16:16:06 +0330

    fx

--- src/helpers/telegram/removeExchangeTelegram.js
+++ src/helpers/telegram/removeExchangeTelegram.js
@@ -5,7 +5,7 @@ import userController from "../../controllers/userController"
 
 function removeExchangeTelegram({message_id, telegram_id, telegram_chat_id, text})
 {
-    const data = text.replace(telegramConstant.removeExchange)
+    const data = text.replace(telegramConstant.removeExchange, "")
 
     userController.getUserByTelegramId({telegram_id})
         .then(user =>
