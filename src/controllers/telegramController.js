d29b010bd6f810ed6de4383871ce9dfba37bfabe
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-01-26 17:01:24 +0330

    fx

--- src/controllers/telegramController.js
+++ src/controllers/telegramController.js
@@ -33,6 +33,7 @@ function handlePvChat({message})
 
 function handleChannelChat({channel_post})
 {
+    console.log(channel_post)
     const {message_id, sender_chat, author_signature, chat, date, text} = channel_post
     const {type, id: telegram_chat_id} = chat
     if (type === "channel")
