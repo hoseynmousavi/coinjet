65c57826876427d3ca248584c61c8a1218141656
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-02-03 19:32:49 +0330

    fx

--- src/controllers/kucoinController.js
+++ src/controllers/kucoinController.js
@@ -43,7 +43,7 @@ function createFutureOrder({userExchange, order: {clientOid, side, symbol, lever
         kuCoinUserExchange: userExchange,
         data: {
             type: "limit", remark: "coinjet bot added this", stopPriceType: "TP",
-            clientOid, side, symbol: symbol, leverage, stop, stopPrice, price, size,
+            clientOid, side, symbol: symbol.toUpperCase(), leverage, stop, stopPrice, price, size,
         },
     })
 }
