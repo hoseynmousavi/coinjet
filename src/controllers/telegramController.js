d7703e22a9ad4b88e0de5e5bcd58a1753e491bee
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-02-03 22:04:20 +0330

    fx

--- src/controllers/telegramController.js
+++ src/controllers/telegramController.js
@@ -58,7 +58,7 @@ function handleChannelChat({channel_post})
             let is_short, is_futures, pair, leverage, entry, target, stop = null
             if (message.includes("short") || message.includes("long")) is_short = message.includes("short")
             if (message.includes("spot") || message.includes("futures")) is_futures = message.includes("futures")
-            pair = message.match(regexConstant.pair)?.[0]?.replace("pair:", "")
+            pair = message.match(regexConstant.pair)?.[0]?.replace("pair:", "")?.toUpperCase()
             leverage = message.match(regexConstant.leverage)?.[0]?.replace("leverage:", "")
             entry = message.match(regexConstant.entry)?.[0]?.replace("entry:", "").split("-")
             target = message.match(regexConstant.target)?.[0]?.replace("target:", "").split("-")
