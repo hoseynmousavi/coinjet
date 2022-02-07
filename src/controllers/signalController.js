cbde1e33491bee72a9bccaa242f6ae3724678f5a
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-02-07 17:24:28 +0330

    fx

--- src/controllers/signalController.js
+++ src/controllers/signalController.js
@@ -37,6 +37,7 @@ function addSignal(signal)
                                                     if (usdtBalance / price > contract.multiplier)
                                                     {
                                                         const size = (usdtBalance / price) / contract.multiplier
+                                                        console.log(usdtBalance / price, contract.multiplier)
                                                         orderController.addOrder({
                                                             user_id: userExchange.user_id,
                                                             signal_id: addedSignal._id,
