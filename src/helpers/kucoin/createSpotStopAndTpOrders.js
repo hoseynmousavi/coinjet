04b42ff1528b7a11b34a15a27c59b5a0c922dbc9
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-05-05 22:41:09 +0430

    fx

--- src/helpers/kucoin/createSpotStopAndTpOrders.js
+++ src/helpers/kucoin/createSpotStopAndTpOrders.js
@@ -101,7 +101,7 @@ async function submitOrders({targets, userExchange, signal_id, size, base_min_si
         {
             tpCount++
             let sizeTemp = index === targets.length - 1 ? remainedSize : Math.min(size, Math.max(base_min_size, +(percent / 100 * size).toFixed(countDecimalPoints(base_increment))))
-            remainedSize = (remainedSize - sizeTemp).toFixed(countDecimalPoints(base_increment))
+            remainedSize = +(remainedSize - sizeTemp).toFixed(countDecimalPoints(base_increment))
             if (remainedSize < base_min_size)
             {
                 sizeTemp = +(sizeTemp + remainedSize).toFixed(countDecimalPoints(base_increment))
